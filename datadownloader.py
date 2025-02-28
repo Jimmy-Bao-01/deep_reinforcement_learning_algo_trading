9ec67651d4ffa53f00c2d7cb1834518f68bf0a7b
Jimmy-Bao-01 <jimmybao2001@gmail.com>
2025-02-28 11:59:27 +0100

    Adding of all data

--- datadownloader.py
+++ datadownloader.py
@@ -7,7 +7,7 @@ import os
 def get_stock_data(ticker, start_date, end_date):
     stock_data = yf.download(ticker, start=start_date, end=end_date)
     stock_data.columns = stock_data.columns.droplevel(1)
-    stock_data.drop(columns=["Adj Close"], inplace=True)
+    # stock_data.drop(columns=["Adj Close"], inplace=True)
     return stock_data
 
 def save_yfinance_data_to_csv(df, start_date, end_date, ticker):
@@ -23,12 +23,44 @@ def save_yfinance_data_to_csv(df, start_date, end_date, ticker):
 if __name__ == "__main__":
     
     tickers = ["AAPL"]
+    stocks = {
+    'Dow Jones' : 'DIA',
+    'S&P 500' : 'SPY',
+    'NASDAQ 100' : 'QQQ',
+    'FTSE 100' : 'EZU',
+    'Nikkei 225' : 'EWJ',
+    'Google' : 'GOOGL',
+    'Apple' : 'AAPL',
+    'Facebook' : 'FB',
+    'Amazon' : 'AMZN',
+    'Microsoft' : 'MSFT',
+    'Twitter' : 'TWTR',
+    'Nokia' : 'NOK',
+    'Philips' : 'PHIA.AS',
+    'Siemens' : 'SIE.DE',
+    'Baidu' : 'BIDU',
+    'Alibaba' : 'BABA',
+    'Tencent' : '0700.HK',
+    'Sony' : '6758.T',
+    'JPMorgan Chase' : 'JPM',
+    'HSBC' : 'HSBC',
+    'CCB' : '0939.HK',
+    'ExxonMobil' : 'XOM',
+    'Shell' : 'RDSA.AS',
+    'PetroChina' : 'PTR',
+    'Tesla' : 'TSLA',
+    'Volkswagen' : 'VOW3.DE',
+    'Toyota' : '7203.T',
+    'Coca Cola' : 'KO',
+    'AB InBev' : 'ABI.BR',
+    'Kirin' : '2503.T'
+    }
     start_date = "2012-01-01"
     start_validation_date = "2016-01-01"
-    splitting_date = "2017-12-31"
-    end_date = "2019-12-31"
+    splitting_date = '2018-01-01'
+    end_date = '2020-01-01'
     
-    for ticker in tickers:
+    for ticker in stocks.values():
         if ticker == "AAPL":
             val_data = get_stock_data(ticker, start_validation_date, splitting_date)
             save_yfinance_data_to_csv(val_data, start_validation_date, splitting_date, ticker)
