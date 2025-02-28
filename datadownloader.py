ed492575758a4a7eab94e57cad9981e29a6c8741
Jimmy-Bao-01 <jimmybao2001@gmail.com>
2025-02-28 19:17:40 +0100

    Start implementation of the trading environment + maj on the dataset

--- datadownloader.py
+++ datadownloader.py
@@ -1,29 +1,21 @@
+"""
+Goal: Downloading stock market data from Yahoo Finance and saving it as CSV files.
+"""
+
+###############################################################################
+################################### Imports ###################################
+###############################################################################
+
 import yfinance as yf
-import pandas as pd
-import numpy as np
 import os
+import pandas as pd
 
+###############################################################################
+################################ Global variables #############################
+###############################################################################
 
-def get_stock_data(ticker, start_date, end_date):
-    stock_data = yf.download(ticker, start=start_date, end=end_date)
-    stock_data.columns = stock_data.columns.droplevel(1)
-    # stock_data.drop(columns=["Adj Close"], inplace=True)
-    return stock_data
-
-def save_yfinance_data_to_csv(df, start_date, end_date, ticker):
-    # Ensure the 'data' directory exists
-    os.makedirs("data", exist_ok=True)
-
-    # Format the filename
-    filename = f"data/{ticker}_{start_date}_{end_date}.csv"
-
-    # Save the DataFrame as CSV
-    df.to_csv(filename, index=True)  # Ensure index (dates) is kept
-
-if __name__ == "__main__":
-    
-    tickers = ["AAPL"]
-    stocks = {
+ # Dictionary listing the 30 stocks considered as testbench
+stocks = {
     'Dow Jones' : 'DIA',
     'S&P 500' : 'SPY',
     'NASDAQ 100' : 'QQQ',
@@ -31,10 +23,9 @@ if __name__ == "__main__":
     'Nikkei 225' : 'EWJ',
     'Google' : 'GOOGL',
     'Apple' : 'AAPL',
-    'Facebook' : 'FB',
+    'Meta' : 'META',
     'Amazon' : 'AMZN',
     'Microsoft' : 'MSFT',
-    'Twitter' : 'TWTR',
     'Nokia' : 'NOK',
     'Philips' : 'PHIA.AS',
     'Siemens' : 'SIE.DE',
@@ -46,25 +37,59 @@ if __name__ == "__main__":
     'HSBC' : 'HSBC',
     'CCB' : '0939.HK',
     'ExxonMobil' : 'XOM',
-    'Shell' : 'RDSA.AS',
-    'PetroChina' : 'PTR',
+    'Shell' : 'SHEL',
+    'PetroChina' : '0857.HK',
     'Tesla' : 'TSLA',
     'Volkswagen' : 'VOW3.DE',
     'Toyota' : '7203.T',
     'Coca Cola' : 'KO',
     'AB InBev' : 'ABI.BR',
-    'Kirin' : '2503.T'
-    }
-    start_date = "2012-01-01"
-    start_validation_date = "2016-01-01"
-    splitting_date = '2018-01-01'
-    end_date = '2020-01-01'
+    'Kirin' : '2503.T',
+    # 'Twitter' : 'TWTR' # No accessible now
+}
+
+# Variables defining the default trading horizon
+start_date = "2012-01-01"
+start_validation_date = "2016-01-01"
+splitting_date = '2018-01-01'
+end_date = '2020-01-01'
+
+def get_stock_data(ticker, start_date, end_date):
+    stock_data = yf.download(ticker, start=start_date, end=end_date)
+    stock_data.columns = stock_data.columns.droplevel(1)
+    # stock_data.set_index(stock_data['Date'], inplace=True)
+    return stock_data
+
+def save_yfinance_data_to_csv(df, start_date, end_date, ticker):
+    # Ensure the 'data' directory exists
+    os.makedirs("data", exist_ok=True)
+
+    # Format the filename
+    filename = f"data/{ticker}_{start_date}_{end_date}.csv"
+
+    # Save the DataFrame as CSV
+    df.to_csv(filename, index=True)  # Ensure index (dates) is kept
     
-    for ticker in stocks.values():
-        if ticker == "AAPL":
-            val_data = get_stock_data(ticker, start_validation_date, splitting_date)
-            save_yfinance_data_to_csv(val_data, start_validation_date, splitting_date, ticker)
-        training_data = get_stock_data(ticker, start_date, splitting_date)
-        save_yfinance_data_to_csv(training_data, start_date, splitting_date, ticker)
-        testing_data = get_stock_data(ticker, splitting_date, end_date)
-        save_yfinance_data_to_csv(testing_data, splitting_date, end_date, ticker)
+def getDailyData(marketSymbol, startingDate, endingDate):
+    """
+    GOAL: Downloding daily stock market data from the Yahoo Finance API. 
+    
+    INPUTS:     - marketSymbol: Stock market symbol.
+                - startingDate: Beginning of the trading horizon.
+                - endingDate: Ending of the trading horizon.
+        
+    OUTPUTS:    - data: Pandas dataframe containing the stock market data.
+    """
+    
+    data = pd.read_csv('data/'+marketSymbol+'_'+startingDate+'_'+endingDate+'.csv')
+    return data
+
+if __name__ == "__main__":
+    for stock in stocks.values():
+        if stock == "AAPL":
+            val_data = get_stock_data(stock, start_validation_date, splitting_date)
+            save_yfinance_data_to_csv(val_data, start_validation_date, splitting_date, stock)
+        training_data = get_stock_data(stock, start_date, splitting_date)
+        save_yfinance_data_to_csv(training_data, start_date, splitting_date, stock)
+        testing_data = get_stock_data(stock, splitting_date, end_date)
+        save_yfinance_data_to_csv(testing_data, splitting_date, end_date, stock)
